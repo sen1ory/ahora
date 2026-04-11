f0a5b2df66a8fe8aad89b481b3d6c20566e471e1
andd1e@tutamail.com <andd1e@tutamail.com>
2026-04-11 10:45:57 +0300

    feat: init project structure

---
+++ app/main/qml/Main.qml
@@ -0,0 +1,9 @@
+import QtQuick
+
+Window {
+    id: root
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr("Hello World")
+}
---
+++ app/main/src/main.cpp
@@ -0,0 +1,12 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[]) {
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+    engine.loadFromModule("Ahora_app_main", "Main");
+    if (engine.rootObjects().isEmpty())
+        exit(-1);
+
+    return app.exec();
+}
